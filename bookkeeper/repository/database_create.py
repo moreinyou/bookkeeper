1edb3c6c03adb7d17545a1f2d444dca17aa51ce4
moreinyou <dmitrievamarina10@yandex.ru>
2024-04-06 23:37:41 +0300

    add file for create db ann add function

---
+++ bookkeeper/repository/database_create.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+class DatabaseConnection():
+    def __init__(self, path):
+        self.conn = sqlite3.connect(path)
+        self.cursor = self.conn.cursor()
+
+    def execute_query(self, query, params=None):
+        if params is not None:
+            self.cursor.execute(query, params)
+        else:
+            self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def close_connection(self):
+        self.conn.close()
+
+
+db_name='lovely.db'
+basa_for_everything = DatabaseConnection(db_name)
+
+basa_for_everything.execute_query('''
+        CREATE TABLE IF NOT EXISTS Category (
+            name TEXT,
+            parent INTEGER,
+            pk INTEGER PRIMARY KEY)''')
+
+basa_for_everything.execute_query('''
+        CREATE TABLE IF NOT EXISTS Expense (
+            amount INTEGER,
+            category INTEGER,
+            expense_date DATE,
+            comment TEXT,
+            pk INTEGER PRIMARY KEY,
+            FOREIGN KEY (category) REFERENCES Category(name))''')
+
+basa_for_everything.execute_query('''
+        CREATE TABLE IF NOT EXISTS Budget (
+            amount INTEGER,
+            category INTEGER,
+            pk INTEGER PRIMARY KEY,
+            term DATE)''')
+
+basa_for_everything.close_connection()
