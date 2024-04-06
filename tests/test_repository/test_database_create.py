d477990af394866bd464571d94ca44a3fe9b1a67
moreinyou <dmitrievamarina10@yandex.ru>
2024-04-07 00:12:09 +0300

    add test to database_create

---
+++ tests/test_repository/test_database_create.py
@@ -0,0 +1,36 @@
+import pytest
+import sqlite3
+from bookkeeper.repository.database_create import DatabaseConnection
+
+@pytest.fixture
+def db_connection():
+    db_name = 'lovely.db'
+    return DatabaseConnection(db_name)
+
+def test_execute_query(db_connection):
+    # Проверяем, что метод execute_query создает таблицу Category
+    db_connection.execute_query('''
+            CREATE TABLE IF NOT EXISTS Category (
+                name TEXT,
+                parent INTEGER,
+                pk INTEGER PRIMARY KEY)''')
+    cursor = db_connection.conn.cursor()
+    cursor.execute('SELECT name FROM sqlite_master WHERE type="table" AND name="Category"')
+    result = cursor.fetchone()
+    assert result is not None
+
+def test_execute_query_with_params(db_connection):
+    # Проверяем, что метод execute_query с параметрами работает корректно
+    query = "INSERT INTO Category (name, parent) VALUES (?, ?)"
+    params = ("Category1", 0)
+    db_connection.execute_query(query, params)
+    cursor = db_connection.conn.cursor()
+    cursor.execute('SELECT name FROM Category WHERE name="Category1"')
+    result = cursor.fetchone()
+    assert result is not None
+
+def test_close_connection(db_connection):
+    # Проверяем, что метод close_connection закрывает соединение с базой данных
+    db_connection.close_connection()
+    with pytest.raises(sqlite3.ProgrammingError):
+        db_connection.execute_query('SELECT 1')
