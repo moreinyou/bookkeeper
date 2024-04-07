a0ec6902e3702a7037b6e969eb1f9a7d9d3f861f
moreinyou <dmitrievamarina10@yandex.ru>
2024-04-07 23:40:49 +0300

    add ne test for budget

--- tests/test_models/test_budget.py
+++ tests/test_models/test_budget.py
@@ -1,35 +1,24 @@
 import pytest
 
-
-from dataclasses import dataclass, field
-from datetime import datetime, timedelta
-
 from bookkeeper.repository.memory_repository import MemoryRepository
 from bookkeeper.models.budget import Budget
-
 
 @pytest.fixture
 def repo():
     return MemoryRepository()
 
-def test_budget_init(repo):
-    budget = Budget(1, 100)
-    assert budget.category == 1
-    assert budget.amount == 100
-    assert budget.term == timedelta(days=1)
-    assert budget.pk == 0
+def test_create_with_full_args_list():
+    b = Budget(period=2, amount=100, pk=1)
+    assert b.period == 2
+    assert b.amount == 100
 
-def test_budget_custom_term(repo):
-    custom_term = timedelta(days=30)
-    budget = Budget(1, 100, term=custom_term)
-    assert budget.category == 1
-    assert budget.amount == 100
-    assert budget.term == custom_term
-    assert budget.pk == 0
+def test_create_brief():
+    b = Budget(1, 100)
+    assert b.period == 1
+    assert b.amount == 100
 
-def test_budget_pk(repo):
-    budget = Budget(1, 100, pk=5)
-    assert budget.category == 1
-    assert budget.amount == 100
-    assert budget.term == timedelta(days=1)
-    assert budget.pk == 5
+
+def test_can_add_to_repo(repo):
+    b = Budget(100, 1)
+    pk = repo.add(b)
+    assert b.pk == pk
